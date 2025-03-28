9e9b53de643186c4b81062640f537d3a454a43f8
aiise <apkyao2009@gmail.com>
2025-03-28 09:00:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+
+		// power
+		function power(){
+			let power =document.getElementById('number').value;
+			if (power === "")
+			window.alert('the answer is ' + Math.pow('power'));
+		}
+
+		// round
+		function round() {
+			let round =(document.getElementById('number').value);
+			window.alert('the answer is ' + Math.round('round'));			
+		}
+
+		// square
+		function square() {
+			let square =(document.getElementById('number').value);
+			window.alert('the answer is ' + Math.sqrt('square'));			
+		}
+
+		// random
+		function random() {
+			let random =(document.getElementById('number').value);
+			window.alert('the answer is ' + Math.random('random'));
+		}
+
+</script>
